fb1bb1cffde9402b99d55573d4934fff24cb6e9b
olsandy <anders.olsson@aalto.fi>
2024-12-10 17:56:44 +0200

    12.21

--- frontend/src/services/persons.js
+++ frontend/src/services/persons.js
@@ -1,25 +1,26 @@
-import axios from 'axios'
+import axios from 'axios';
 //const baseUrl = 'http://localhost:3001/persons'
-const baseUrl = '/api/persons'
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
+const baseUrl = '/api/persons';
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then((response) => response.data)
-}
+  const request = axios.get(baseUrl);
+  return request.then(response => response.data);
+};
 
-const create = (newObject) => {
-  const request = axios.post(baseUrl, newObject)
-  return request.then((response) => response.data)
-}
+const create = newObject => {
+  const request = axios.post(baseUrl, newObject);
+  return request.then(response => response.data);
+};
 
-const update = (newObject) => {
-  const request = axios.put(`${baseUrl}/${newObject.id}`, newObject)
-  return request.then((response) => response.data)
-}
+const update = newObject => {
+  const request = axios.put(`${baseUrl}/${newObject.id}`, newObject);
+  return request.then(response => response.data);
+};
 
-const remove = (id) => {
-  const request = axios.delete(`${baseUrl}/${id}`)
-  return request.then((response) => response.data)
-}
+const remove = id => {
+  const request = axios.delete(`${baseUrl}/${id}`);
+  return request.then(response => response.data);
+};
 
-export default { create, getAll, remove, update }
+export default { create, getAll, remove, update };
